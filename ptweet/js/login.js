a8a0c4a731b6e7d886efbaae978a567a9ef8bdc4
Oscarmal27 <71733530+Oscarmal27@users.noreply.github.com>
2021-11-05 19:31:20 -0700

    PTweet 051121

---
+++ ptweet/js/login.js
@@ -0,0 +1,42 @@
+window.onload= function(){
+    var boton = document.getElementById("btnAceptar");
+    var txtUsuario = document.getElementById("txtUsuario");
+    var txtPassword = document.getElementById("txtPassword");
+
+    boton.addEventListener('click',function(){
+        let usuario = txtUsuario.value;
+        let pass = txtPassword.value;
+        let arregloUsuarios=[
+            {id:1, nombre:"Federica Pluche", image:"fede.jpg", username:"fedep", password:"123"},
+            {id:1, nombre:"Don Camerino", image:"dc.jpg", username:"camerino", password:"123"},
+            {id:1, nombre:"Vivi", image:"vivi.jpg", username:"vivi", password:"123"},
+
+        ];
+        let encontro=false;
+
+        arregloUsuarios.forEach(fila=>{
+            if(fila.username == usuario.trim() && fila.password == pass.trim()){
+                encontro=true;
+                localStorage.setItem("usuario", JSON.stringify(fila));
+                let fecha=new Date();
+                localStorage.setItem("fecha", fecha.getFullYear()+"/"+fecha.getMonth()+"/"+fecha.getDay());
+            }
+        });
+        if(encontro){
+            ///redireccionar
+            location.href="./index.html";
+            
+        }else{
+            document.getElementById("error").innerHTML="Credenciales Incorrectas";
+            txtUsuario.style.backgroundColor="#FF0000";
+            txtPassword.style.backgroundColor="#FF0000";
+        }
+
+    });
+
+}
+function limpiar(){
+   
+    txtUsuario.style.backgroundColor="#FFF"
+    txtPassword.style.backgroundColor="#FFF"
+}
